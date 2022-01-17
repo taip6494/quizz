b5cc68a77b3ddb8c4e937d23b5e9ea35fc08a395
taip6494 <palisseauthomas@gmail.com>
2022-01-17 13:48:57 +0400

    4 - Les questions

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,11 @@ class _body extends StatefulWidget {
   @override
   State<_body> createState() => body();
 }
+class Question {
+  String question;
+  bool reponseCorrect;
+  Question(this.question, this.reponseCorrect);
+}
 
 class body extends State<_body> {
   final check = Icon(Icons.check, color: Colors.green);
@@ -24,9 +29,26 @@ class body extends State<_body> {
   int count = 0;
   List<Icon> listeIcone = [];
 
+  final List<Question> questionsListe = [
+    Question('Test 1 ',false),
+    Question('Test 2 ', true),
+    Question('Test 3', true),
+    Question('Test 4', true),
+    Question('Test 5', true),
+    Question('Test 6', true),
+    Question('Test 7', false),
+    Question('Test 9', true),
+    Question('Test 10', false),
+    Question('Test 11', true),
+    Question('Test 12', true),
+    Question('Test 13', true),
+    Question('Test 14', true),
+  ];
+
   void ajoutItems(item){
     setState(() {
       listeIcone.add(item);
+      questionsList.add
     });
   }
   @override
@@ -96,4 +118,6 @@ class body extends State<_body> {
     );
   }
   }
+
+
 
