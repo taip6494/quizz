10245c3820b87cb415a7d5167daa2da051513864
taip6494 <palisseauthomas@gmail.com>
2022-01-17 12:46:00 +0400

    3bis - update la liste

--- lib/main.dart
+++ lib/main.dart
@@ -23,65 +23,77 @@ class body extends State<_body> {
   final close = Icon(Icons.clear, color: Colors.red);
   int count = 0;
   List<Icon> listeIcone = [];
+
+  void ajoutItems(item){
+    setState(() {
+      listeIcone.add(item);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 5,
-          child:
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Center(
-            child: Text(
-              'Ceci est une question',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 5,
+            child:
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  'Ceci est une question',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
             ),
           ),
-        ),
-        ),
-        Expanded(
+          Expanded(
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: (
-          ElevatedButton(
-              child: Text(
-                'Vrai',
-                style: TextStyle(color: Colors.white, fontSize: 20),
+                  ElevatedButton(
+                      child: Text(
+                        'Vrai',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green, // background (button) color
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: () {
+                        ajoutItems(check);
+                      }
+
+                  )
               ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // background (button) color
-                onPrimary: Colors.white,
-              ),
-              onPressed: () {},
-          )
-    ),
-            ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: (
-                ElevatedButton(
-                  child: Text(
-                    'faux',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red, // background (button) color
-                    onPrimary: Colors.white,
-                  ),
-                  onPressed: () {},
-                )
             ),
           ),
-        ),
-        Row(
-          children:
-          listeIcone,
-        )
-    ]
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: (
+                  ElevatedButton(
+                    child: Text(
+                      'faux',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // background (button) color
+                      onPrimary: Colors.white,
+                    ),
+                    onPressed: () {
+                      ajoutItems(close);
+                    },
+                  )
+              ),
+            ),
+          ),
+          Row(
+            children:
+            listeIcone,
+          )
+        ]
     );
+  }
   }
 
